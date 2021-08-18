1e706b1e7e8767815d171e390c2398830d924395
ORARAPA <87627137+ORARAPA@users.noreply.github.com>
2021-08-18 18:20:24 +0800

    [ToDoList][Paraluman] feat: add mockapis

---
+++ todo-list-redux/src/features/apis/index.js
@@ -0,0 +1,8 @@
+import axios from "axios";
+
+const api = axios.create({
+    baseURL:'https://611cd22f7d273a0017e2f43c.mockapi.io/',
+});
+
+
+export default api;
---
+++ todo-list-redux/src/features/apis/todos.js
@@ -0,0 +1,19 @@
+import api from "./index";
+
+export const getTodos = () => {
+    return api.get("/todos");
+}
+
+
+export const addTodo = (text) => {
+    return api.post("/todos",{text})
+}
+
+
+export const deleteTodo = (id) => {
+    return api.delete(`/todos/${id}`)
+}
+
+export const updateTodo = (id,updateTodo) => {
+    return api.put(`/todos/${id}`,updateTodo)
+}
