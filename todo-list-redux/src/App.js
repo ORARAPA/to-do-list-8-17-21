6388aff7228b456293dd0f07457d3395436dfd40
ORARAPA <87627137+ORARAPA@users.noreply.github.com>
2021-08-18 19:00:03 +0800

    [ToDoList][Paraluman] feat: apply antd and create home page

--- todo-list-redux/src/App.js
+++ todo-list-redux/src/App.js
@@ -3,25 +3,41 @@ import TodoList from './features/todos/components/TodoList';
 import {Route, Link, BrowserRouter, Switch, HashRouter} from "react-router-dom"
 import NotFoundPage from './features/notfound/NotFoundPage';
 import DoneList from './features/todos/components/DoneList';
+import { Layout, Menu, Breadcrumb } from 'antd';
+import Home from './features/todos/components/Home';
+const { Header, Content, Footer } = Layout;
 
 function App() {
   return (
     <div className="App">
-      <BrowserRouter>
+    <BrowserRouter>
+      <Layout className="layout">
+        <Content style={{ padding: '0 50px' }}>
+          <Header>
+            <div className="logo" />
+            <Menu theme="dark" mode="horizontal" defaultSelectedKeys={['1']}>
+              <Menu.Item key={1}><Link to="/">Home</Link></Menu.Item>
+              <Menu.Item><Link to="/todos">To-do List</Link></Menu.Item>
+              <Menu.Item><Link to="/done">Done List</Link></Menu.Item>
+            </Menu>
+          </Header>
+          <Switch>
+            <Route exact path="/" component={Home}></Route>
+            <Route exact path="/todos" component={TodoList}></Route>
+            <Route path="/done" component={DoneList}></Route>
+            <Route path="*" component={NotFoundPage}></Route>
+          </Switch>
+        </Content>
+        <Footer style={{ textAlign: 'center' }}>Ant Design ©2018 Created by Ant UED</Footer>
+      </Layout>
+    </BrowserRouter>
       
-        <ul>
-        <Link to="/todos">To-do List</Link>
-        &nbsp;&nbsp;&nbsp;
-        <Link to="/done">Done List</Link>
-        </ul>
-        <Switch>
-          <Route exact path="/todos" component={TodoList}></Route>
-          <Route path="/done" component={DoneList}></Route>
-          <Route path="*" component={NotFoundPage}></Route>
-        </Switch>
-      </BrowserRouter>
+     
     </div>
   );
 }
 
 export default App;
+
+ 
+
---
+++ todo-list-redux/src/features/todos/components/Home.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import "../styles/Home.css";
+
+function Home() {
+    return (
+        <div>
+            <h1>Welcome to your To do List!</h1>
+            <h2>Keep track of your tasks, be alert at all times.</h2>
+        </div>
+    )
+}
+
+export default Home
