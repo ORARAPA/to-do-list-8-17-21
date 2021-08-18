7321fb8fe9ee3ffd93655d72d77ba987f2cbe7de
ORARAPA <87627137+ORARAPA@users.noreply.github.com>
2021-08-18 18:19:59 +0800

    [ToDoList][Paraluman] feat: addtodos in todolist

--- todo-list-redux/src/features/todos/components/TodoList.js
+++ todo-list-redux/src/features/todos/components/TodoList.js
@@ -2,14 +2,28 @@ import React from 'react';
 import TodoForm from './TodoForm';
 import TodoGroup from './TodoGroup';
 import "../styles/TodoList.css";
+import { AddTodos, selectToDoIds } from '../reducers/todosSlice';
+import {useEffect} from "react";
+import { getTodos } from '../../apis/todos';
+import { useDispatch, useSelector } from "react-redux";
 
 
 function TodoList(){
+    const todoIds = useSelector(selectToDoIds)
+    const dispatch = useDispatch();
+
+    useEffect(() => {
+        getTodos().then((response) => {
+        console.log("response.data: ", response.data)
+        dispatch(AddTodos(response.data));
+        });
+    }, []);
+
     return (
         <div>
             <h1>To do List:</h1>
-            <TodoGroup />
             <TodoForm />
+            <TodoGroup />
         </div>
     )
 }
