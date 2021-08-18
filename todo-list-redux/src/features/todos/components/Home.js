b1bcabbd4c3820c82d2071ef35817d90d24f0d93
ORARAPA <87627137+ORARAPA@users.noreply.github.com>
2021-08-18 19:03:52 +0800

    [ToDoList][Paraluman] refactor: modify title

--- todo-list-redux/src/features/todos/components/Home.js
+++ todo-list-redux/src/features/todos/components/Home.js
@@ -4,7 +4,7 @@ import "../styles/Home.css";
 function Home() {
     return (
         <div>
-            <h1>Welcome to your To do List!</h1>
+            <h1 className="title">Welcome to your To do List!</h1>
             <h2>Keep track of your tasks, be alert at all times.</h2>
         </div>
     )
